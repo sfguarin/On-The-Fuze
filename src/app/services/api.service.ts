b5622797a451097bfa1e50ef51346fbbf7f009f4
Stiven Guarin <sf.guarin@gmail.com>
2022-07-25 08:33:58 -0500

    On The Fuze Test v1.4 funcionando

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,27 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  private baseUrl: string = environment.baseUrl;
+
   constructor(private http : HttpClient) { }
 
   //Endpoints de comunicación con el backend
   postUser(data: any){
-    return this.http.post<any>("http://localhost:8080/api/usuarios", data);
+    return this.http.post<any>(this.baseUrl+"/api/usuarios", data);
   }
 
   getUser(){
-    return this.http.get<any>("http://localhost:8080/api/usuarios");
+    return this.http.get<any>(this.baseUrl+"/api/usuarios");
   }
 
   putUser(data:any, id : number){
-    return this.http.put<any>("http://localhost:8080/api/usuarios/"+id, data);
+    return this.http.put<any>(this.baseUrl+"/api/usuarios/"+id, data);
   }
 
   deleteUser(id : number){
-    return this.http.delete<any>("http://localhost:8080/api/usuarios/"+id);
+    return this.http.delete<any>(this.baseUrl+"/api/usuarios/"+id);
   }
 }
